57818996df0a2cf3259b42f7e4a20113267f2f05
BittorTelletxea <btellechea22wg@ikzubirimanteo.com>
2024-01-15 13:13:28 +0100

    Autentifikazio orria

--- resources/js/Components/konponenteak/Pasahitza.jsx
+++ resources/js/Components/konponenteak/Pasahitza.jsx
@@ -1,16 +1,9 @@
-import { useEffect } from 'react';
-import Checkbox from '@/Components/Checkbox';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
+import { useState, useEffect } from 'react';
 import { Head, Link, useForm } from '@inertiajs/react';
 import logo from "../images/logoL.png";
 import "../style/login.css";
 import { HeaderL } from './HeaderL';
 import { FooterL } from './FooterL';
-
 
 export function Pasahitza({ status, canResetPassword }) {
     const { data, setData, post, processing, errors, reset } = useForm({
@@ -19,47 +12,52 @@ export function Pasahitza({ status, canResetPassword }) {
         remember: false,
     });
 
+    // Estado del contador
+    const [counter, setCounter] = useState(600); // 10 minutos en segundos
+
     useEffect(() => {
-        return () => {
-            reset('password');
-        };
+        const interval = setInterval(() => {
+            // Decrementar el contador cada segundo
+            setCounter((prevCounter) => prevCounter - 1);
+        }, 1000);
+
+        // Limpiar el intervalo cuando el componente se desmonta
+        return () => clearInterval(interval);
     }, []);
 
-  
+    // Formatear el tiempo restante en minutos y segundos
+    const minutes = Math.floor(counter / 60);
+    const seconds = counter % 60;
 
- 
-   return (
-       <div className='dena bg-light'>
-           <section className="vh-100 bg-light">
-               <header>
-                   <div className="howmuch h-100 mt-3">
-                       <a className="navbar-brand text-black display-1 fw-bolder text-center" href="/">
-                           <img src={logo} width="50" height="40" alt="" />
-                           How Much
-                       </a>
-                   </div>
-               </header>
-               <div className="container h-100">
-                   <div className="row d-flex justify-content-center align-items-center h-75">
-                       <div className="col col-xl-10">
-                           <div className="card p-lg-5 text-black">
-                           <form >
-                <h5 className="fw-normal mb-3 pb-3" style={{ letterSpacing: '1px' }}>Pasahitz berreskurapena</h5>
-                <div className="mb-4">
-                    <label className="form-label" htmlFor="form2Example17">Email helbidea</label>
-                    <input type="text" className="form-control" id="email" name="email" placeholder="email@email.com" />
-                    <p className='mezua'>Zure email helbidea sartu eta gero konfirmazio bat bidaliko zaizu</p>
+    return (
+        <div className='dena bg-light'>
+            <section className="vh-100 bg-light">
+                <header>
+                    <div className="howmuch h-100 mt-3">
+                        <a className="navbar-brand text-black display-1 fw-bolder text-center" href="/">
+                            <img src={logo} width="50" height="40" alt="" />
+                            How Much
+                        </a>
+                    </div>
+                </header>
+                <div className="container h-100">
+                    <div className="row d-flex justify-content-center align-items-center h-75">
+                        <div className="col col-xl-10">
+                            <div className="card p-lg-5 text-black">
+                                <form>
+                                    <h5 className="fw-normal mb-3 pb-3" style={{ letterSpacing: '1px' }}>Autentifikatu</h5>
+                                    <div className="mb-4">
+                                        <p className="text-black" htmlFor="form2Example17">Autentikazio mezua zure email helbidera ailegatu da, ea zu zaren jakiteko</p>
+                                        <p className='contador'>{`${minutes}:${seconds < 10 ? '0' : ''}${seconds}`}</p>
+                                    </div>
+                                    <div className="pt-1 mb-4">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="pt-1 mb-4">
-                </div>
-            </form>
-            <a href="/Konfirmatu"><button className="btn btn-dark btn-lg btn-block" type="submit">KONFIRMATU</button></a>
-
-                           </div>
-                       </div>
-                   </div>
-               </div>
-           </section>
-       </div>
-   );
+            </section>
+        </div>
+    );
 }
