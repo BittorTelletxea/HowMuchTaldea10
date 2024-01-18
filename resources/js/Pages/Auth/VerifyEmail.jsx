34268320d3a0aeea835f30f2af71acc1884104eb
BittorTelletxea <btellechea22wg@ikzubirimanteo.com>
2024-01-18 09:55:29 +0100

    Autentifikazioa orria

--- resources/js/Pages/Auth/VerifyEmail.jsx
+++ resources/js/Pages/Auth/VerifyEmail.jsx
@@ -1,45 +1,42 @@
 import GuestLayout from '@/Layouts/GuestLayout';
 import PrimaryButton from '@/Components/PrimaryButton';
 import { Head, Link, useForm } from '@inertiajs/react';
+import LogoutButton from '@/Components/konponenteak/LogoutButton';
+import '../../Components/style/headerl.css';
+import logo from "../../Components/images/logoL.png";
+
 
 export default function VerifyEmail({ status }) {
     const { post, processing } = useForm({});
 
     const submit = (e) => {
         e.preventDefault();
-
         post(route('verification.send'));
     };
 
     return (
-        <GuestLayout>
+        <div className='bg-light flex items-center justify-center h-screen'>
+            <header>
+                   <div className="howmuch h-100 mt-3 text-center">
+                       <a className="navbar-brand text-black display-1 fw-bolder text-center" href="/">
+                           <img src={logo} width="50" height="40" alt="" />
+                           How Much
+                       </a>
+                   </div>
+               </header>
             <Head title="Email Verification" />
 
-            <div className="mb-4 text-sm text-gray-600">
-                Thanks for signing up! Before getting started, could you verify your email address by clicking on the
-                link we just emailed to you? If you didn't receive the email, we will gladly send you another.
-            </div>
-
-            {status === 'verification-link-sent' && (
-                <div className="mb-4 font-medium text-sm text-green-600">
-                    A new verification link has been sent to the email address you provided during registration.
+            <form onSubmit={submit} className='verify card'>
+                <div className="mb-4 text-sm text-gray-600">
+                    <h4 className='text-center mt-1'><b>Autentifikazioa</b></h4>
+                    <p className='text-center'>Eskerrik asko kontua sortzeagatik, ea zu zaren jakiteko email bat bidali dizugu</p>
                 </div>
-            )}
-
-            <form onSubmit={submit}>
-                <div className="mt-4 flex items-center justify-between">
-                    <PrimaryButton disabled={processing}>Resend Verification Email</PrimaryButton>
-
-                    <Link
-                        href={route('logout')}
-                        method="post"
-                        as="button"
-                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        Log Out
-                    </Link>
+                <div className="mt-4 flex items-center ">
+                    <button disabled={processing} className='berbidali btn btn-dark'>
+                        Berbidali email-a
+                    </button>
                 </div>
             </form>
-        </GuestLayout>
+        </div>
     );
 }
